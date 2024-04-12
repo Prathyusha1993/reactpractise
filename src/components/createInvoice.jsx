e4177f54f7551367e654b410d3c434432b01588e
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-04-11 17:17:32 -0700

    editing table cells

--- src/components/createInvoice.jsx
+++ src/components/createInvoice.jsx
@@ -17,17 +17,73 @@ const CreateInvoice = ({
     <div>
       {/* <h4>Create Invoices</h4> */}
       <form>
-        <label>ClientName</label>
-        <input type="text" value={clientName} onChange={handleClientName} />
-        <label>DateCreation</label>
-        <input type="text" value={dateCreation} onChange={handleDateCreation} />
-        <label>referenceNum</label>
-        <input type="text" value={referenceNum} onChange={handleReferenceNum} />
-        <label>amount</label>
-        <input type="text" value={amount} onChange={handleAmount} />
-        <label>status</label>
-        <input type="text" value={status} onChange={handleStatus} />
-        <button onClick={handleSubmit}> Submit</button>
+        <label style={{ fontSize: "14px" }}>ClientName</label>
+        <input
+          style={{
+            marginLeft: "4px",
+            marginRight: "7px",
+            borderRadius: "4px",
+            border: "0.5px solid balck",
+          }}
+          type="text"
+          value={clientName}
+          onChange={handleClientName}
+        />
+        <label style={{ fontSize: "14px" }}>DateCreation</label>
+        <input
+          style={{
+            marginLeft: "4px",
+            marginRight: "7px",
+            borderRadius: "4px",
+            border: "0.5px solid balck",
+          }}
+          type="text"
+          value={dateCreation}
+          onChange={handleDateCreation}
+        />
+        <label style={{ fontSize: "14px" }}>referenceNum</label>
+        <input
+          style={{
+            marginLeft: "4px",
+            marginRight: "7px",
+            borderRadius: "4px",
+            border: "0.5px solid balck",
+          }}
+          type="text"
+          value={referenceNum}
+          onChange={handleReferenceNum}
+        />
+        <label style={{ fontSize: "14px" }}>amount</label>
+        <input
+          style={{
+            marginLeft: "4px",
+            marginRight: "7px",
+            borderRadius: "4px",
+            border: "0.5px solid balck",
+          }}
+          type="text"
+          value={amount}
+          onChange={handleAmount}
+        />
+        <label style={{ fontSize: "14px" }}>status</label>
+        <input
+          style={{
+            marginLeft: "4px",
+            marginRight: "7px",
+            borderRadius: "4px",
+            border: "0.5px solid balck",
+          }}
+          type="text"
+          value={status}
+          onChange={handleStatus}
+        />
+        <button
+          style={{ color: "black", backgroundColor: "lightgreen" }}
+          onClick={handleSubmit}
+        >
+          {" "}
+          Submit
+        </button>
       </form>
     </div>
   );
--- src/components/listOfInvoices.jsx
+++ src/components/listOfInvoices.jsx
@@ -8,6 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import listInvoices from "../listOfInvoices.json";
 import CreateInvoice from "./createInvoice";
+import { ClickAwayListener, TextField } from "@mui/material";
 
 const ListOfInvoices = () => {
   const [invoices, setInvoies] = useState(listInvoices);
@@ -16,6 +17,8 @@ const ListOfInvoices = () => {
   const [referenceNum, setReferenceNum] = useState("");
   const [amount, setAmount] = useState("");
   const [status, setStatus] = useState("");
+  const [rowIndex, setRowIndex] = useState(-1);
+  const [columnIndex, setColumnIndex] = useState(-1);
 
   const handleClientName = (e) => {
     e.preventDefault();
@@ -68,6 +71,15 @@ const ListOfInvoices = () => {
     setInvoies(updatedInvoices);
   };
 
+  const handleTextFieldChange = (rowInd, colName, value) => {
+    invoices[rowInd][colName] = value;
+  };
+
+  const handleExit = () => {
+    setRowIndex(-1);
+    setColumnIndex(-1);
+  };
+
   return (
     <div>
       <h4>This shows list of invoices</h4>
@@ -87,38 +99,127 @@ const ListOfInvoices = () => {
         />
       </div>
       <div>
-        <TableContainer
-          component={Paper}
-          style={{ border: ".2px solid black", margin: "15px" }}
-        >
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Client Name</TableCell>
-                <TableCell align="right">Date Creation</TableCell>
-                <TableCell align="right">Reference Number</TableCell>
-                <TableCell align="right">Amount</TableCell>
-                <TableCell align="right">Invoice Status</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {invoices.map((row) => (
-                <TableRow
-                  key={row.clientName}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.clientName}
+        <ClickAwayListener onClickAway={() => handleExit()}>
+          <TableContainer
+            component={Paper}
+            style={{ border: ".2px solid black", margin: "15px" }}
+          >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontWeight: "bold" }}>
+                    Client Name
                   </TableCell>
-                  <TableCell align="right">{row.dateCreation}</TableCell>
-                  <TableCell align="right">{row.referenceNum}</TableCell>
-                  <TableCell align="right">{row.amount}</TableCell>
-                  <TableCell align="right">{row.status}</TableCell>
+                  <TableCell style={{ fontWeight: "bold" }} align="right">
+                    Date Creation
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }} align="right">
+                    Reference Number
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }} align="right">
+                    Amount
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }} align="right">
+                    Invoice Status
+                  </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {invoices.map((row, index) => (
+                  <TableRow
+                    key={row.clientName}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      onClick={() => {
+                        setRowIndex(index);
+                        setColumnIndex(0);
+                      }}
+                    >
+                      {rowIndex === index && columnIndex === 0 ? (
+                        <TextField
+                          defaultValue={invoices[index]["clientName"]}
+                          onChange={(e) =>
+                            handleTextFieldChange(
+                              index,
+                              "clientName",
+                              e.target.value
+                            )
+                          }
+                          onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                              handleExit();
+                            }
+                          }}
+                        />
+                      ) : (
+                        row.clientName
+                      )}
+                    </TableCell>
+                    <TableCell
+                      onClick={() => {
+                        setRowIndex(index);
+                        setColumnIndex(0);
+                      }}
+                      align="right"
+                    >
+                      {rowIndex === index && columnIndex === 0 ? (
+                        <TextField
+                          defaultValue={invoices[index]["dateCreation"]}
+                          onChange={(e) =>
+                            handleTextFieldChange(
+                              index,
+                              "dateCreation",
+                              e.target.value
+                            )
+                          }
+                          onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                              handleExit();
+                            }
+                          }}
+                        />
+                      ) : (
+                        row.dateCreation
+                      )}
+                    </TableCell>
+                    <TableCell align="right">{row.referenceNum}</TableCell>
+                    <TableCell
+                      onClick={() => {
+                        setRowIndex(index);
+                        setColumnIndex(0);
+                      }}
+                      align="right"
+                    >
+                      {rowIndex === index && columnIndex === 0 ? (
+                        <TextField
+                          defaultValue={invoices[index]["amount"]}
+                          onChange={(e) =>
+                            handleTextFieldChange(
+                              index,
+                              "amount",
+                              e.target.value
+                            )
+                          }
+                          onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                              handleExit();
+                            }
+                          }}
+                        />
+                      ) : (
+                        row.amount
+                      )}
+                    </TableCell>
+                    <TableCell align="right">{row.status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </ClickAwayListener>
       </div>
     </div>
   );
